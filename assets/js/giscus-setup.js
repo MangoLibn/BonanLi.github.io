6c6779cf4e421512d8503fa25b70ab405e664d3a
MangoLibn <libonan@ucas.ac.cn>
2025-10-23 10:26:40 +0000

    Deploying to gh-pages from @ MangoLibn/MangoLibn.github.io@a902f5e6051a20c8eeee12ab8c1135030cb207b1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "MangoLibn/BonanLi.github.io",
+    "data-repo": "MangoLibn/MangoLibn.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
